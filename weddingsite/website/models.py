ebd85ef8241ea4b608bca721d4c1cbbe7196353b
JonaThanPabonP <71296819+JonaThanPabonP@users.noreply.github.com>
2022-05-19 00:04:27 -0500

    Page base created

--- weddingsite/website/models.py
+++ weddingsite/website/models.py
@@ -1,3 +1,19 @@
+from distutils.command.upload import upload
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.nombre
+
+class Foto(models.Model):
+    id = models.AutoField(primary_key=True)
+    categoria = models.ForeignKey('Categoria', on_delete=models.CASCADE)
+    imagen = models.ImageField(null=True)
+
+    def __str__(self) -> str:
+        return f"Foto{self.id} ({self.categoria})"
--- weddingsite/website/views.py
+++ weddingsite/website/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Categoria, Foto
 
 # Create your views here.
+def index(request):
+    
+    return render(request, 'index.html')
+
+def fotos(request):
+    fts = Foto.objects.all()
+    context = {
+        "fotos": fts,
+    }
+    return render(request, 'index.html', context)
